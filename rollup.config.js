812b4a24d81426c5e549393524bdc19bff6c9441
Etienne Burdet <etienne.burdet@gmail.com>
2020-12-03 15:56:22 +0100

    Add Eslint (standard)

--- rollup.config.js
+++ rollup.config.js
@@ -1,153 +1,153 @@
-import path from 'path';
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import commonjs from '@rollup/plugin-commonjs';
-import url from '@rollup/plugin-url';
-import svelte from 'rollup-plugin-svelte';
-import babel from '@rollup/plugin-babel';
+import path from 'path'
+import resolve from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+import commonjs from '@rollup/plugin-commonjs'
+import url from '@rollup/plugin-url'
+import svelte from 'rollup-plugin-svelte'
+import babel from '@rollup/plugin-babel'
 import alias from '@rollup/plugin-alias'
-import { terser } from 'rollup-plugin-terser';
-import scss from 'rollup-plugin-scss';
-import sveltePreprocess from 'svelte-preprocess';
-import config from 'sapper/config/rollup.js';
-import pkg from './package.json';
+import { terser } from 'rollup-plugin-terser'
+import scss from 'rollup-plugin-scss'
+import sveltePreprocess from 'svelte-preprocess'
+import config from 'sapper/config/rollup.js'
+import pkg from './package.json'
 
 import postcss from 'postcss'
 import autoprefixer from 'autoprefixer'
 import purgecss from '@fullhuman/postcss-purgecss'
 
-const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
-const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+const mode = process.env.NODE_ENV
+const dev = mode === 'development'
+const legacy = !!process.env.SAPPER_LEGACY_BUILD
 
 const onwarn = (warning, onwarn) =>
-	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
-	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
-	onwarn(warning);
+  (warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
+  (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
+  onwarn(warning)
 
-const entries = { '@': path.resolve(__dirname, 'src') }
+const entries = { '@': path.resolve(__dirname, 'src') }
 
 const postcssConfig = [
-	autoprefixer,
-	purgecss(({
-		content: ['src/**/*.svelte']
-	}))
+  autoprefixer,
+  purgecss(({
+    content: ['src/**/*.svelte']
+  }))
 ]
 
 const preprocess = sveltePreprocess({
-	scss: {
-		renderSync: true
-	},
-	postcss: {
-		plugins: postcssConfig
-	}
+  scss: {
+    renderSync: true
+  },
+  postcss: {
+    plugins: postcssConfig
+  }
 })
 
 const scssPlugin = scss({
-	output: 'static/global.css',
-	watch: 'src/styles',
-	processor: css => postcss(postcssConfig)
+  output: 'static/global.css',
+  watch: 'src/styles',
+  processor: css => postcss(postcssConfig)
 })
 
 export default {
-	client: {
-		input: config.client.input(),
-		output: config.client.output(),
-		plugins: [
-			alias({entries}),
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			scssPlugin,
-			svelte({
-				preprocess,
-				dev,
-				hydratable: true,
-				emitCss: true
-			}),
-			url({
-				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
-				publicPath: '/client/'
-			}),
-			resolve({
-				browser: true,
-				dedupe: ['svelte']
-			}),
-			commonjs(),
+  client: {
+    input: config.client.input(),
+    output: config.client.output(),
+    plugins: [
+      alias({ entries }),
+      replace({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode)
+      }),
+      scssPlugin,
+      svelte({
+        preprocess,
+        dev,
+        hydratable: true,
+        emitCss: true
+      }),
+      url({
+        sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
+        publicPath: '/client/'
+      }),
+      resolve({
+        browser: true,
+        dedupe: ['svelte']
+      }),
+      commonjs(),
 
-			legacy && babel({
-				extensions: ['.js', '.mjs', '.html', '.svelte'],
-				babelHelpers: 'runtime',
-				exclude: ['node_modules/@babel/**'],
-				presets: [
-					['@babel/preset-env', {
-						targets: '> 0.25%, not dead'
-					}]
-				],
-				plugins: [
-					'@babel/plugin-syntax-dynamic-import',
-					['@babel/plugin-transform-runtime', {
-						useESModules: true
-					}]
-				]
-			}),
+      legacy && babel({
+        extensions: ['.js', '.mjs', '.html', '.svelte'],
+        babelHelpers: 'runtime',
+        exclude: ['node_modules/@babel/**'],
+        presets: [
+          ['@babel/preset-env', {
+            targets: '> 0.25%, not dead'
+          }]
+        ],
+        plugins: [
+          '@babel/plugin-syntax-dynamic-import',
+          ['@babel/plugin-transform-runtime', {
+            useESModules: true
+          }]
+        ]
+      }),
 
-			!dev && terser({
-				module: true
-			})
-		],
+      !dev && terser({
+        module: true
+      })
+    ],
 
-		preserveEntrySignatures: false,
-		onwarn,
-	},
+    preserveEntrySignatures: false,
+    onwarn
+  },
 
-	server: {
-		input: config.server.input(),
-		output: config.server.output(),
-		plugins: [
-			alias({entries}),
-			replace({
-				'process.browser': false,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			scssPlugin,
-			svelte({
-				preprocess,
-				generate: 'ssr',
-				hydratable: true,
-				dev
-			}),
-			url({
-				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
-				publicPath: '/client/',
-				emitFiles: false // already emitted by client build
-			}),
-			resolve({
-				dedupe: ['svelte']
-			}),
-			commonjs()
-		],
-		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
+  server: {
+    input: config.server.input(),
+    output: config.server.output(),
+    plugins: [
+      alias({ entries }),
+      replace({
+        'process.browser': false,
+        'process.env.NODE_ENV': JSON.stringify(mode)
+      }),
+      scssPlugin,
+      svelte({
+        preprocess,
+        generate: 'ssr',
+        hydratable: true,
+        dev
+      }),
+      url({
+        sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
+        publicPath: '/client/',
+        emitFiles: false // already emitted by client build
+      }),
+      resolve({
+        dedupe: ['svelte']
+      }),
+      commonjs()
+    ],
+    external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
 
-		preserveEntrySignatures: 'strict',
-		onwarn,
-	},
+    preserveEntrySignatures: 'strict',
+    onwarn
+  },
 
-	serviceworker: {
-		input: config.serviceworker.input(),
-		output: config.serviceworker.output(),
-		plugins: [
-			resolve(),
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			commonjs(),
-			!dev && terser()
-		],
+  serviceworker: {
+    input: config.serviceworker.input(),
+    output: config.serviceworker.output(),
+    plugins: [
+      resolve(),
+      replace({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode)
+      }),
+      commonjs(),
+      !dev && terser()
+    ],
 
-		preserveEntrySignatures: false,
-		onwarn,
-	}
-};
+    preserveEntrySignatures: false,
+    onwarn
+  }
+}
