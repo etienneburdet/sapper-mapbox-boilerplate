bab799cfead66e80aa96077760893e06e098657b
Frédéric Passaniti <frederic.passaniti@opendatasoft.com>
2020-12-11 08:46:23 +0100

    export for deployment

---
+++ __sapper__/export/client/index.ded89aff.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as e}from"./client.fe6509d5.js";async function r(s,t){return this.redirect(301,"trees/0")}export default class extends s{constructor(s){super(),t(this,s,null,null,e,{})}}export{r as preload};
