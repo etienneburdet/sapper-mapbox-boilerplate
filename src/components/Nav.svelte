f14647b5247826909a50a42317570099f6d1f6b9
Etienne Burdet <etienne.burdet@gmail.com>
2020-12-12 13:22:15 +0100

    Added top load bar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,183 +1,185 @@
 <script>
-    import {onMount} from "svelte";
+  import { onMount } from 'svelte';
 
-    export let segment;
-    let open = false;
-    let openbtn;
-    let closeBtn;
-    let ul;
+  export let segment;
+  let open = false;
+  let openbtn;
+  let closeBtn;
+  let ul;
 
-    onMount(() => {
-        openbtn.addEventListener('click', () => {
-            open = true;
-        });
-        closeBtn.addEventListener('click', () => {
-            open = false;
-        });
-        ul.addEventListener('click', (e) => {
-            if (e.target.tagName == 'A') {
-                open = false;
-            }
-        });
+  onMount(() => {
+    ul.addEventListener('click', (e) => {
+      if (e.target.tagName == 'A') {
+        open = false;
+      }
     });
+  });
 </script>
 
 <nav class:open>
-    <div class="header-container is-flex is-justify-content-center is-align-items-center is-mobile">
-        <img class="header-logo" src="/mstile-150x150.png"/>
-        <div class="header-menu">
-            <div class="header-menu-top">
-                <p>MENU</p>
-                <i class="header-menu-close fas fa-times" bind:this={closeBtn}></i>
-            </div>
-            <ul bind:this={ul}>
-                <li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-                <li><a aria-current="{segment === 'trees' ? 'page' : undefined}" href="trees">trees</a></li>
-                <li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
+  <div class="header-container is-flex is-justify-content-center is-align-items-center is-mobile">
+    <img class="header-logo" src="/mstile-150x150.png" />
+    <div class="header-menu">
+      <div class="header-menu-top">
+        <p>MENU</p>
+        <i class="header-menu-close fas fa-times" on:click={(open = false)} />
+      </div>
+      <ul bind:this={ul}>
+        <li><a aria-current={segment === undefined ? 'page' : undefined} href=".">home</a></li>
+        <li><a aria-current={segment === 'trees' ? 'page' : undefined} href="trees">trees</a></li>
+        <li><a aria-current={segment === 'about' ? 'page' : undefined} href="about">about</a></li>
 
-                <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+        <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
                      the blog data when we hover over the link or tap it on a touchscreen -->
-                <li><a rel=prefetch aria-current="{segment === 'partners' ? 'page' : undefined}" href="partners">partners</a>
-                </li>
-            </ul>
-        </div>
-        <div class="burger is-align-items-center is-mobile">
-            <a bind:this={openbtn}
-               role="button" class="navbar-burger burger" aria-label="menu" aria-expanded={open?true:false}>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-        </div>
+        <li>
+          <a
+            on:click={(open = true)}
+            rel="prefetch"
+            aria-current={segment === 'partners' ? 'page' : undefined}
+            href="partners"
+          >partners</a>
+        </li>
+      </ul>
     </div>
-
-
+    <div class="burger is-align-items-center is-mobile">
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded={open ? true : false}
+      >
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+      </a>
+    </div>
+  </div>
 </nav>
 
 <style lang="scss">
-    @import "../styles/global.scss";
+  @import '../styles/global.scss';
 
-    nav {
-        position: relative;
+  nav {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 70px;
+    font-weight: 300;
+    padding: 0 1em;
+    z-index: 10;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
+
+    .burger {
+      display: none;
+      position: absolute;
+      right: 0;
+
+      .navbar-burger {
+        height: 50px;
+        width: 50px;
+      }
+    }
+
+    .header-container {
+      display: flex;
+      align-items: center;
+
+      .header-logo {
+        height: 50px;
+      }
+
+      .header-menu-top {
+        display: none;
+
+        .header-menu-close {
+          font-size: 1.8em;
+          margin-bottom: 3px;
+          cursor: pointer;
+        }
+      }
+
+      ul {
         display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 70px;
-        font-weight: 300;
-        padding: 0 1em;
-        z-index: 10;
-        box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+        margin: 0;
+        padding: 0;
 
-        .burger {
-            display: none;
-            position: absolute;
-            right: 0;
+        li {
+          font-size: 0.9em;
+          padding-left: 30px;
 
-            .navbar-burger {
-                height: 50px;
-                width: 50px;
+          a {
+            padding: 0;
+            color: black;
+            text-decoration: none;
+
+            &:hover {
+              text-decoration: underline;
             }
+          }
         }
-
-        .header-container {
-            display: flex;
-            align-items: center;
-
-            .header-logo {
-                height: 50px;
-            }
-
-            .header-menu-top {
-                display: none;
-
-                .header-menu-close {
-                    font-size: 1.8em;
-                    margin-bottom: 3px;
-                    cursor: pointer;
-                }
-            }
-
-            ul {
-                display: flex;
-                margin: 0;
-                padding: 0;
-
-                li {
-                    font-size: .9em;
-                    padding-left: 30px;
-
-                    a {
-                        padding: 0;
-                        color: black;
-                        text-decoration: none;
-
-                        &:hover {
-                            text-decoration: underline;
-                        }
-                    }
-                }
-            }
-        }
+      }
     }
+  }
 
-    @media screen and (max-width: $mobile) {
-        nav {
-            height: 50px;
+  @media screen and (max-width: $mobile) {
+    nav {
+      height: 50px;
 
-            .burger {
-                display: flex;
-            }
+      .burger {
+        display: flex;
+      }
 
-            .header-container {
-                .header-logo {
-                    height: 30px;
-                }
-
-                ul {
-                    display: none;
-                    margin: 60px 0;
-
-                    li {
-                        font-size: 1.2em;
-                        font-weight: 400;
-                        padding-left: 0;
-
-                        &:not(:first-child) {
-                            padding-top: 30px;
-                        }
-                    }
-                }
-            }
+      .header-container {
+        .header-logo {
+          height: 30px;
         }
-    }
 
-    @media screen and (max-width: $mobile) {
-        nav.open {
-            .header-menu {
-                .header-menu-top {
-                    display: flex;
-                    justify-content: space-between;
-                }
+        ul {
+          display: none;
+          margin: 60px 0;
 
-                display: flex;
-                flex-direction: column;
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                background: white;
-                z-index: 100;
-                padding: 15px 30px;
+          li {
+            font-size: 1.2em;
+            font-weight: 400;
+            padding-left: 0;
 
-                ul {
-                    display: flex;
-                    flex-direction: column;
-                }
+            &:not(:first-child) {
+              padding-top: 30px;
             }
+          }
         }
+      }
     }
+  }
 
-    /*[aria-current] {
+  @media screen and (max-width: $mobile) {
+    nav.open {
+      .header-menu {
+        .header-menu-top {
+          display: flex;
+          justify-content: space-between;
+        }
+
+        display: flex;
+        flex-direction: column;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        background: white;
+        z-index: 100;
+        padding: 15px 30px;
+
+        ul {
+          display: flex;
+          flex-direction: column;
+        }
+      }
+    }
+  }
+
+  /*[aria-current] {
         position: relative;
         display: inline-block;
     }
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,55 +1,56 @@
 <script>
-    import {stores} from '@sapper/app';
-    import Nav from '@/components/Nav.svelte';
-    import Spinner from '@/components/Spinner.svelte';
+  import { stores } from '@sapper/app';
+  import Nav from '@/components/Nav.svelte';
+  import LoadBar from '@/components/LoadBar.svelte';
 
-    const {preloading} = stores;
+  const { preloading } = stores();
 
-    export let segment;
+  export let segment;
 </script>
 
 <svelte:head>
-    <meta name="msapplication-TileColor" content="#ffffff"/>
-    <meta name="theme-color" content="#ffffff"/>
+  <meta name="msapplication-TileColor" content="#ffffff" />
+  <meta name="theme-color" content="#ffffff" />
 
-    <meta name="description" content="A base sapper app for list over maps with calls to ods API">
-    <meta property="og:title" content="ODS Sapper Map Template">
-    <meta property="og:description" content="A base sapper app for list over maps with calls to ods API">
-    <meta property="og:image" content="TODO">
-    <meta property="og:url" content="TOD">
-    <meta name="twitter:card" content="TODO">
+  <meta name="description" content="A base sapper app for list over maps with calls to ods API" />
+  <meta property="og:title" content="ODS Sapper Map Template" />
+  <meta
+    property="og:description"
+    content="A base sapper app for list over maps with calls to ods API"
+  />
+  <meta property="og:image" content="TODO" />
+  <meta property="og:url" content="TOD" />
+  <meta name="twitter:card" content="TODO" />
 
-    <meta property="og:site_name" content="Opendatasoft">
-    <meta name="twitter:image:alt" content="Alt text for image">
-    <meta property="fb:app_id" content="your_app_id"/>
-    <meta name="twitter:site" content="@website-username">
-
-    <style src="../styles/global.scss"></style>
+  <meta property="og:site_name" content="Opendatasoft" />
+  <meta name="twitter:image:alt" content="Alt text for image" />
+  <meta property="fb:app_id" content="your_app_id" />
+  <meta name="twitter:site" content="@website-username" />
+  <style src="../styles/global.scss">
+  </style>
 </svelte:head>
+{#if $preloading}
+  <LoadBar />
+{/if}
 
-<Nav {segment}/>
+<Nav {segment} />
 
 <main>
-    {#if $preloading}
-        {console.log('spinner')}
-        <div class="button is-loading"></div>
-    {:else}
-        <slot></slot>
-    {/if}
+  <slot />
 </main>
 
 <style lang="scss">
-    main {
-        position: relative;
-        background-color: white;
-        box-sizing: border-box;
-        width: 100%;
-        height: calc(100% - 70px);
-    }
+  main {
+    position: relative;
+    background-color: white;
+    box-sizing: border-box;
+    width: 100%;
+    height: calc(100% - 70px);
+  }
 
-    @media screen and (max-width: 769px) {
-        main {
-            height: calc(100% - 50px);
-        }
+  @media screen and (max-width: 769px) {
+    main {
+      height: calc(100% - 50px);
     }
+  }
 </style>
