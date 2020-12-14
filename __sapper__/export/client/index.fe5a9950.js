64836624e372e31cb49bf52b5e385d967ddfe2f4
Etienne Burdet <etienne.burdet@gmail.com>
2020-12-14 16:57:58 +0100

    export

---
+++ __sapper__/export/client/index.fe5a9950.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as r}from"./client.4b677b73.js";async function e(s,t){return this.redirect(301,"farms/all")}export default class extends s{constructor(s){super(),t(this,s,null,null,r,{})}}export{e as preload};
