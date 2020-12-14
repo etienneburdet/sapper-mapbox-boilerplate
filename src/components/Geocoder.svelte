14a3884b0d18e28346384246be49dabbc2a0cb29
Etienne Burdet <etienne.burdet@gmail.com>
2020-12-14 18:58:35 +0100

    add separate mode for geolocator

--- src/components/Geocoder.svelte
+++ src/components/Geocoder.svelte
@@ -18,6 +18,7 @@
   let query;
   let input;
   let hiddenInput;
+  let ac;
 
   const selectAll = () => {
     input.select();
@@ -30,9 +31,10 @@
   };
 
   onMount(async () => {
+    console.log('mount');
     const autoCompleteModule = await import('@tarekraafat/autocomplete.js');
     const autoComplete = autoCompleteModule.default;
-    const ac = new autoComplete({
+    ac = new autoComplete({
       data: {
         src: async () => {
           const { lat, lng } = center;
@@ -52,7 +54,7 @@
         cache: false,
       },
       placeHolder: placeholder,
-      selector: '#' + id,
+      selector: `#${id}`,
       threshold: 2,
       debounce: 200,
       trigger: ['input', 'focus'],
@@ -96,11 +98,16 @@
   });
 </script>
 
-<div id="search-container-{id}" class="field jawg-geocoder" class:has-addons={geolocator}>
-  <div class="control is-expanded has-icon-left">
+<div
+  id="search-container-{id}"
+  class="field jawg-geocoder is-flex-grow-1"
+  class:has-addons={geolocator === 'add-on'}
+  class:is-grouped={geolocator === 'separate'}
+>
+  <div class="control is-expanded ml-3">
     <input
       {id}
-      class="input"
+      class="input is-fullwidth"
       type="text"
       autocomplete="off"
       bind:value={query}
--- src/routes/farms/[id].svelte
+++ src/routes/farms/[id].svelte
@@ -59,7 +59,7 @@
   <div id="list-ctn" class:mobile-open={toggleList}>
     <!-- DESKTOP LIST HEADER -->
     <div id="list-ctn-header" class="custom-styled-select">
-      <Geocoder id="desktopsearchbox" geolocator={true} on:geocode={filterPage} />
+      <Geocoder id="desktopsearchbox" geolocator="add-on" on:geocode={filterPage} />
 
       <div
         id="list-ctn-header-btn"
@@ -106,12 +106,10 @@
         {#if !toggleList}<span>Liste</span>{/if}
         {#if toggleList}<span>Carte</span>{/if}
       </button>
-      <div id="map-header-content">
-        <Geocoder id="mobilesearchbox" on:geocode={filterPage} />
-      </div>
-      <div class="geolocator">
+      <Geocoder id="mobilesearchbox" on:geocode={filterPage} geolocator="separate" />
+      <!-- <div class="geolocator">
         <Geolocator on:geolocate={filterPage} />
-      </div>
+      </div> -->
     </div>
 
     <!-- MAP FOOTER / FILTERS -->
