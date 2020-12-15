21844e8de4fb8d7598fe13417af5d10ee09428fc
Etienne Burdet <etienne.burdet@gmail.com>
2020-12-15 12:46:02 +0100

    build for deploy

--- client/index.56830401.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as t,s as r}from"./client.12a2e161.js";async function e(s,t){return this.redirect(301,"farms/all")}export default class extends s{constructor(s){super(),t(this,s,null,null,r,{})}}export{e as preload};
