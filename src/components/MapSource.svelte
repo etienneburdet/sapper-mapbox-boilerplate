12ae1311afbbb45554eb275def97d4a0acb34e96
Etienne Burdet <etienne.burdet@gmail.com>
2020-12-16 15:42:54 +0100

    Stop loader on sourcedata loaded

--- src/components/MapSource.svelte
+++ src/components/MapSource.svelte
@@ -1,81 +1,96 @@
 <script>
-    import { getContext, onMount, setContext } from 'svelte';
-    import { fetchGeojson } from '@/routes/farms/_helpers';
+  import { getContext, onMount, setContext } from 'svelte';
+  import { fetchGeojson } from '@/routes/farms/_helpers';
 
-    import { stores } from '@sapper/app';
-    import Spinner from './Spinner.svelte';
-    import { filterQueryParams } from '../routes/farms/_helpers';
+  import { stores } from '@sapper/app';
+  import Spinner from './Spinner.svelte';
+  import { filterQueryParams } from '../routes/farms/_helpers';
 
-    const { page, session } = stores();
+  const { page, session } = stores();
 
-    export let dataUrl;
-    export let id;
+  export let dataUrl;
+  export let id;
 
-    const { getMap } = getContext('map');
-    const map = getMap();
+  const { getMap } = getContext('map');
+  const map = getMap();
 
-    let isSourceLoaded = false;
-    let spin = false;
-    let farmsGeojson;
-    let lastQueryParams = false;
+  let isSourceLoaded = false;
+  let loading = false;
+  let farmsGeojson;
+  let lastQueryParams = false;
 
-    setContext('source', {
-        getMapSourceId: () => id,
-    });
+  map.on('sourcedata', () => (loading = false));
 
-    const setSource = (data) => {
-        if (!data) return;
-        spin = true;
-        if (map.getSource(id)) {
-            map.getSource(id)
-                    .setData(data);
-        } else {
-            map.addSource(id, {
-                type: 'geojson',
-                data,
-            });
-        }
-        isSourceLoaded = true;
-        spin = false;
-    };
+  setContext('source', {
+    getMapSourceId: () => id,
+  });
 
-    const destroySource = () => {
-        const { layers } = map.getStyle();
-        layers.forEach((layer) => map.removeLayer(layer.id));
-
-        if (map.getSource(id)) {
-            map.removeSource(id);
-        }
-        isSourceLoaded = false;
-    };
-
-    // When url change -> refetch the data
-    $: {
-        let filteredQueryParams = filterQueryParams($page.query);
-        if (!lastQueryParams || filteredQueryParams !== lastQueryParams) {
-            spin = true;
-            fetchGeojson($page)
-                    .then((res) => {
-                        farmsGeojson = res;
-                    });
-            lastQueryParams = filteredQueryParams;
-        }
-    }
-
-    // when data changed, refresh the source
-    $: if (map.isStyleLoaded()) {
-        setSource(farmsGeojson);
+  const setSource = (data) => {
+    if (!data) return;
+    if (map.getSource(id)) {
+      map.getSource(id).setData(data);
     } else {
-        map.on('load', () => setSource(farmsGeojson));
+      map.addSource(id, {
+        type: 'geojson',
+        data,
+      });
     }
+    isSourceLoaded = true;
+  };
 
-    onMount(() => {
-        return destroySource;
-    });
+  const destroySource = () => {
+    const { layers } = map.getStyle();
+    layers.forEach((layer) => map.removeLayer(layer.id));
+
+    if (map.getSource(id)) {
+      map.removeSource(id);
+    }
+    isSourceLoaded = false;
+  };
+
+  // When url change -> refetch the data
+  $: {
+    let filteredQueryParams = filterQueryParams($page.query);
+    if (!lastQueryParams || filteredQueryParams !== lastQueryParams) {
+      loading = true;
+      fetchGeojson($page).then((res) => {
+        farmsGeojson = res;
+      });
+      lastQueryParams = filteredQueryParams;
+    }
+  }
+
+  // when data changed, refresh the source
+  $: if (map.isStyleLoaded()) {
+    setSource(farmsGeojson);
+  } else {
+    map.on('load', () => setSource(farmsGeojson));
+  }
+
+  onMount(() => {
+    return destroySource;
+  });
 </script>
 
 {#if isSourceLoaded}
-    <slot/>
+  <slot />
 {/if}
 
-<Spinner {spin}/>
+<div class="wrapper">
+  <div class="control" class:is-loading={loading} />
+</div>
+
+<style lang="scss">
+  .wrapper {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  div.control.is-loading::after {
+    height: 10rem;
+    width: 10rem;
+  }
+</style>
--- src/routes/farms/[id].svelte
+++ src/routes/farms/[id].svelte
@@ -1,370 +1,367 @@
 <script context="module">
-    import { farmFacetsUrl, getFarmWhere } from './_helpers';
+  import { farmFacetsUrl, getFarmWhere } from './_helpers';
 
-    export async function preload(page, session) {
-        const { id } = page.params;
-        const { query } = page;
+  export async function preload(page, session) {
+    const { id } = page.params;
+    const { query } = page;
 
-        let farmDetails;
-        if (id !== 'all') {
-            // With 'where'
-            // const farmUrl = getFarmWhere(`add_nom_ferme like "${id}"`);
-            // With 'record/:id'
-            const farmUrl = getFarmRecord(id);
-            const resFromAPI = await this.fetch(farmUrl);
-            const jsonFromAPI = await resFromAPI.json();
-            farmDetails = jsonFromAPI.record;
-        }
-
-        let { facets } = session;
-        if (!facets) {
-            const resFromAPI = await this.fetch(farmFacetsUrl);
-            const jsonFacets = await resFromAPI.json();
-            if (jsonFacets) {
-                let facetsObj = {};
-                jsonFacets.facets.forEach((facet) => {
-                    facetsObj[facet.name] = facet.facets;
-                });
-                facets = facetsObj;
-                session.facets = facets;
-            }
-        }
-
-        return {
-            farmDetails,
-            facets,
-            query,
-        };
+    let farmDetails;
+    if (id !== 'all') {
+      const farmUrl = getFarmRecord(id);
+      const resFromAPI = await this.fetch(farmUrl);
+      const jsonFromAPI = await resFromAPI.json();
+      farmDetails = jsonFromAPI.record;
     }
+
+    let { facets } = session;
+    if (!facets) {
+      const resFromAPI = await this.fetch(farmFacetsUrl);
+      const jsonFacets = await resFromAPI.json();
+      if (jsonFacets) {
+        let facetsObj = {};
+        jsonFacets.facets.forEach((facet) => {
+          facetsObj[facet.name] = facet.facets;
+        });
+        facets = facetsObj;
+        session.facets = facets;
+      }
+    }
+
+    return {
+      farmDetails,
+      facets,
+      query,
+    };
+  }
 </script>
 
 <script>
-    import { goto } from '@sapper/app';
-    import { onMount } from 'svelte';
+  import { goto } from '@sapper/app';
+  import { onMount } from 'svelte';
 
-    import Filter from '@/components/Filter.svelte';
-    import Geocoder from '@/components/Geocoder.svelte';
-    import Map from '@/components/Map.svelte';
-    import MapSource from '@/components/MapSource.svelte';
-    import MapLayer from '@/components/MapLayer.svelte';
-    import Marker from '@/components/Marker.svelte';
-    import Popup from '@/components/Popup.svelte';
-    import List from '@/components/List.svelte';
-    import ListItem from '@/components/ListItem.svelte';
-    import Geolocator from '@/components/Geolocator.svelte';
-    import AdvancedFilters from '@/components/AdvancedFilters.svelte';
+  import Filter from '@/components/Filter.svelte';
+  import Geocoder from '@/components/Geocoder.svelte';
+  import Map from '@/components/Map.svelte';
+  import MapSource from '@/components/MapSource.svelte';
+  import MapLayer from '@/components/MapLayer.svelte';
+  import Marker from '@/components/Marker.svelte';
+  import Popup from '@/components/Popup.svelte';
+  import List from '@/components/List.svelte';
+  import ListItem from '@/components/ListItem.svelte';
+  import Geolocator from '@/components/Geolocator.svelte';
+  import AdvancedFilters from '@/components/AdvancedFilters.svelte';
 
-    import { paint } from './_mapstyle';
-    import { q2center, setActivePoint, filterPage, searchPage, getFarmRecord } from './_helpers';
-    import { stores } from '@sapper/app';
-    const { page } = stores();
+  import { paint } from './_mapstyle';
+  import { q2center, setActivePoint, filterPage, searchPage, getFarmRecord } from './_helpers';
+  import { stores } from '@sapper/app';
+  const { page } = stores();
 
-    let queryparams;
-    let farmsShortlist = [];
-    export let farmDetails;
-    export let facets;
-    export let query;
+  let queryparams;
+  let farmsShortlist = [];
+  export let farmDetails;
+  export let facets;
+  export let query;
 
-    let toggleList = false;
-    let showAdvFilters = false;
-    let showMobileAdvFilters = false;
+  let toggleList = false;
+  let showAdvFilters = false;
+  let showMobileAdvFilters = false;
 
-    $: queryparams = new URLSearchParams($page.query);
+  $: queryparams = new URLSearchParams($page.query);
 </script>
 
 <div id="page-ctn">
-    <div id="list-ctn" class:mobile-open={toggleList}>
-        <!-- DESKTOP LIST HEADER -->
-        <div id="list-ctn-header" class="custom-styled-select">
-            <Geocoder id="desktopsearchbox" geolocator="add-on" on:geocode={filterPage}/>
+  <div id="list-ctn" class:mobile-open={toggleList}>
+    <!-- DESKTOP LIST HEADER -->
+    <div id="list-ctn-header" class="custom-styled-select">
+      <Geocoder id="desktopsearchbox" geolocator="add-on" on:geocode={filterPage} />
 
-            <div
-                    id="list-ctn-header-btn"
-                    class:show-adv-filters={showAdvFilters}
-                    on:click={() => (showAdvFilters = !showAdvFilters)}
-            >
-                Affiner la recherche
-                <i class="fas fas-arrow-up"/>
-            </div>
+      <div
+        id="list-ctn-header-btn"
+        class:show-adv-filters={showAdvFilters}
+        on:click={() => (showAdvFilters = !showAdvFilters)}
+      >
+        Affiner la recherche
+        <i class="fas fas-arrow-up" />
+      </div>
 
-            <div class="adv-filters-ctn" class:show-adv-filters={showAdvFilters}>
-                <AdvancedFilters {facets}/>
-            </div>
-        </div>
-        <!-- DESKTOP LIST -->
-        <div id="list-ctn-content">
-            <List>
-              {#each farmsShortlist as farm (farm)}
-                  <ListItem
-                          id={farm.properties.recordid}
-                          fields={farm.properties}
-                          geometry={farm.geometry}
-                          active={farmDetails && (farm.properties.recordid === farmDetails.id)}
-                  />
-              {/each}
-            </List>
-        </div>
+      <div class="adv-filters-ctn" class:show-adv-filters={showAdvFilters}>
+        <AdvancedFilters {facets} />
+      </div>
     </div>
+    <!-- DESKTOP LIST -->
+    <div id="list-ctn-content">
+      <List>
+        {#each farmsShortlist as farm (farm)}
+          <ListItem
+            id={farm.properties.recordid}
+            fields={farm.properties}
+            geometry={farm.geometry}
+            active={farmDetails && farm.properties.recordid === farmDetails.id}
+          />
+        {/each}
+      </List>
+    </div>
+  </div>
 
-    <div id="map-ctn">
-        <!-- MOBILE HEADER -->
-        <div id="map-header">
-            <button class="button is-dark" on:click={() => (toggleList = !toggleList)}>
-              {#if !toggleList}<span>Liste</span>{/if}
-              {#if toggleList}<span>Carte</span>{/if}
-            </button>
-            <Geocoder id="mobilesearchbox" on:geocode={filterPage} geolocator="separate"/>
-            <!-- <div class="geolocator">
+  <div id="map-ctn">
+    <!-- MOBILE HEADER -->
+    <div id="map-header">
+      <button class="button is-dark" on:click={() => (toggleList = !toggleList)}>
+        {#if !toggleList}<span>Liste</span>{/if}
+        {#if toggleList}<span>Carte</span>{/if}
+      </button>
+      <Geocoder id="mobilesearchbox" on:geocode={filterPage} geolocator="separate" />
+      <!-- <div class="geolocator">
               <Geolocator on:geolocate={filterPage} />
             </div> -->
-        </div>
-
-        <!-- MAP FOOTER / FILTERS -->
-        <div id="map-footer">
-            <button
-                    class="button is-rounded is-dark mb-3"
-                    id="map-footer-filters-btn"
-                    class:show-adv-filters={showMobileAdvFilters}
-                    on:click={() => (showMobileAdvFilters = true)}
-            >
-                Affiner les résultats
-            </button>
-            <div class="adv-filters-ctn" class:show-adv-filters={showMobileAdvFilters}>
-                <div class="adv-filters-ctn-top">
-                    <p>Affiner les résultats</p>
-                    <i
-                            class="header-menu-close fas fa-times"
-                            on:click={() => (showMobileAdvFilters = false)}
-                    />
-                </div>
-                <AdvancedFilters {facets}/>
-            </div>
-        </div>
-
-        <!-- MAP -->
-        <Map navigationPosition="bottom-right" center={q2center(query.location)}>
-            <MapSource id="farms">
-                <MapLayer
-                        id="farms-circles"
-                        type="circle"
-                        {paint}
-                        on:mapClick={setActivePoint(queryparams)}
-                        on:render={(event) => (farmsShortlist = event.detail)}
-                />
-            </MapSource>
-          {#if query.marker}
-              <Marker center={q2center(query.marker)} options={{'color':'blue'}}/>
-          {/if}
-
-        {#if query.location && query.location != query.marker}
-            <Marker center={q2center(query.location)} options={{'color':'#74CABF'}}/>
-        {/if}
-        </Map>
     </div>
 
-    <!-- POP UP -->
-    <div id="popup-ctn" class:open={farmDetails}>
-      {#if farmDetails}
-          <Popup item={farmDetails}/>
+    <!-- MAP FOOTER / FILTERS -->
+    <div id="map-footer">
+      <button
+        class="button is-rounded is-dark mb-3"
+        id="map-footer-filters-btn"
+        class:show-adv-filters={showMobileAdvFilters}
+        on:click={() => (showMobileAdvFilters = true)}
+      >
+        Affiner les résultats
+      </button>
+      <div class="adv-filters-ctn" class:show-adv-filters={showMobileAdvFilters}>
+        <div class="adv-filters-ctn-top">
+          <p>Affiner les résultats</p>
+          <i
+            class="header-menu-close fas fa-times"
+            on:click={() => (showMobileAdvFilters = false)}
+          />
+        </div>
+        <AdvancedFilters {facets} />
+      </div>
+    </div>
+
+    <!-- MAP -->
+    <Map navigationPosition="bottom-right" center={q2center(query.location)}>
+      <MapSource id="farms">
+        <MapLayer
+          id="farms-circles"
+          type="circle"
+          {paint}
+          on:mapClick={setActivePoint(queryparams)}
+          on:render={(event) => (farmsShortlist = event.detail)}
+        />
+      </MapSource>
+      {#if query.marker}
+        <Marker center={q2center(query.marker)} options={{ color: 'blue' }} />
       {/if}
-    </div>
+
+      {#if query.location && query.location != query.marker}
+        <Marker center={q2center(query.location)} options={{ color: '#74CABF' }} />
+      {/if}
+    </Map>
+  </div>
+
+  <!-- POP UP -->
+  <div id="popup-ctn" class:open={farmDetails}>
+    {#if farmDetails}
+      <Popup item={farmDetails} />
+    {/if}
+  </div>
 </div>
 
 <style lang="scss">
-    #page-ctn {
+  #page-ctn {
+    display: flex;
+    height: 100%;
+  }
+
+  #list-ctn {
+    display: flex;
+    flex-direction: column;
+    width: 374px;
+    z-index: 2;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
+
+    #list-ctn-header {
+      background-color: black;
+      color: white;
+      position: relative;
+      padding: 20px;
+
+      #list-ctn-header-btn {
         display: flex;
-        height: 100%;
+        align-items: center;
+        cursor: pointer;
+
+        &:after {
+          border: 2px solid white;
+          border-radius: 1.5px;
+          border-right: 0;
+          border-top: 0;
+          content: ' ';
+          display: block;
+          height: 0.625em;
+          margin-left: 5px;
+          transform: rotate(135deg);
+          transform-origin: center;
+          width: 0.625em;
+        }
+
+        &.show-adv-filters:after {
+          transform: rotate(-45deg);
+        }
+      }
+
+      .adv-filters-ctn {
+        display: none;
+        margin-top: 10px;
+
+        &.show-adv-filters {
+          display: block;
+        }
+      }
     }
 
+    #list-ctn-content {
+      flex: 1;
+      overflow: scroll;
+      background: white;
+    }
+
+    :global(&.open) {
+      left: 0;
+    }
+  }
+
+  #map-ctn {
+    width: calc(100% - 374px);
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+
+    #map-header {
+      display: none;
+
+      position: absolute;
+      z-index: 1;
+      padding: 10px;
+      height: 57px;
+      width: 100%;
+
+      #map-header-content {
+        flex: 1;
+        margin: 0 5px;
+      }
+    }
+
+    #map-footer {
+      display: none;
+
+      position: absolute;
+      bottom: 0px;
+      z-index: 1;
+      width: 100%;
+      justify-content: center;
+      align-content: center;
+
+      #map-footer-filters-btn {
+        &.show-adv-filters {
+          display: none;
+        }
+      }
+
+      .adv-filters-ctn {
+        display: none;
+        width: 100%;
+        padding: 22px 20px;
+        border-top-left-radius: 15px;
+        border-top-right-radius: 15px;
+        background-color: black;
+        color: white;
+        box-shadow: 0px -6px 13px rgba(0, 0, 0, 0.15);
+
+        .adv-filters-ctn-top {
+          display: flex;
+          justify-content: space-between;
+
+          p {
+            font-size: 1.2em;
+            font-weight: bold;
+            margin-bottom: 20px;
+          }
+
+          .header-menu-close {
+            font-size: 1.2em;
+            cursor: pointer;
+          }
+        }
+
+        &.show-adv-filters {
+          display: block;
+        }
+      }
+    }
+  }
+
+  #popup-ctn {
+    position: fixed;
+    left: -395px;
+    width: 395px;
+    height: 100%;
+    overflow-y: auto;
+    background: white;
+    border-left: 1px solid #dddddd;
+    transition: 0.2s ease left;
+    z-index: 1;
+    padding: 20px;
+
+    &.open {
+      left: 374px;
+    }
+  }
+
+  @media screen and (max-width: 769px) {
     #list-ctn {
-        display: flex;
-        flex-direction: column;
-        width: 374px;
-        z-index: 2;
-        box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
+      position: absolute;
+      left: -100%;
+      bottom: 0;
+      width: 100%;
+      z-index: 1;
+      background: white;
+      height: calc(100% - 55px);
+      transition: 0.2s ease left;
+
+      &.mobile-open {
+        left: 0;
 
         #list-ctn-header {
-            background-color: black;
-            color: white;
-            position: relative;
-            padding: 20px;
-
-            #list-ctn-header-btn {
-                display: flex;
-                align-items: center;
-                cursor: pointer;
-
-                &:after {
-                    border: 2px solid white;
-                    border-radius: 1.5px;
-                    border-right: 0;
-                    border-top: 0;
-                    content: ' ';
-                    display: block;
-                    height: 0.625em;
-                    margin-left: 5px;
-                    transform: rotate(135deg);
-                    transform-origin: center;
-                    width: 0.625em;
-                }
-
-                &.show-adv-filters:after {
-                    transform: rotate(-45deg);
-                }
-            }
-
-            .adv-filters-ctn {
-                display: none;
-                margin-top: 10px;
-
-                &.show-adv-filters {
-                    display: block;
-                }
-            }
+          display: none;
         }
-
-        #list-ctn-content {
-            flex: 1;
-            overflow: scroll;
-            background: white;
-        }
-
-        :global(&.open) {
-            left: 0;
-        }
+      }
     }
 
     #map-ctn {
-        width: calc(100% - 374px);
-        height: 100%;
+      width: 100%;
+
+      #map-header {
         display: flex;
-        flex-direction: column;
+        align-items: center;
+      }
 
-        #map-header {
-            display: none;
-
-            position: absolute;
-            z-index: 1;
-            padding: 10px;
-            height: 57px;
-            width: 100%;
-
-            #map-header-content {
-                flex: 1;
-                margin: 0 5px;
-            }
-        }
-
-        #map-footer {
-            display: none;
-
-            position: absolute;
-            bottom: 0px;
-            z-index: 1;
-            width: 100%;
-            justify-content: center;
-            align-content: center;
-
-            #map-footer-filters-btn {
-                &.show-adv-filters {
-                    display: none;
-                }
-            }
-
-            .adv-filters-ctn {
-                display: none;
-                width: 100%;
-                padding: 22px 20px;
-                border-top-left-radius: 15px;
-                border-top-right-radius: 15px;
-                background-color: black;
-                color: white;
-                box-shadow: 0px -6px 13px rgba(0, 0, 0, 0.15);
-
-                .adv-filters-ctn-top {
-                    display: flex;
-                    justify-content: space-between;
-
-                    p {
-                        font-size: 1.2em;
-                        font-weight: bold;
-                        margin-bottom: 20px;
-                    }
-
-                    .header-menu-close {
-                        font-size: 1.2em;
-                        cursor: pointer;
-                    }
-                }
-
-                &.show-adv-filters {
-                    display: block;
-                }
-            }
-        }
+      #map-footer {
+        display: flex;
+      }
     }
 
     #popup-ctn {
-        position: fixed;
-        left: -395px;
-        width: 395px;
-        height: 100%;
-        overflow-y: auto;
-        background: white;
-        border-left: 1px solid #dddddd;
-        transition: 0.2s ease left;
-        z-index: 1;
-        padding: 20px;
+      left: 0;
+      bottom: -70%;
+      height: 70%;
+      width: 100%;
+      transition: 0.2s ease bottom;
 
-        &.open {
-            left: 374px;
-        }
+      &.open {
+        left: 0;
+        bottom: 0;
+      }
     }
-
-    @media screen and (max-width: 769px) {
-        #list-ctn {
-            position: absolute;
-            left: -100%;
-            bottom: 0;
-            width: 100%;
-            z-index: 1;
-            background: white;
-            height: calc(100% - 55px);
-            transition: 0.2s ease left;
-
-            &.mobile-open {
-                left: 0;
-
-                #list-ctn-header {
-                    display: none;
-                }
-            }
-        }
-
-        #map-ctn {
-            width: 100%;
-
-            #map-header {
-                display: flex;
-                align-items: center;
-            }
-
-            #map-footer {
-                display: flex;
-            }
-        }
-
-        #popup-ctn {
-            left: 0;
-            bottom: -70%;
-            height: 70%;
-            width: 100%;
-            transition: 0.2s ease bottom;
-
-            &.open {
-                left: 0;
-                bottom: 0;
-            }
-        }
-    }
+  }
 </style>
