c0e467f6d6a49b9aa45fdbbe1d754403a08d0aa7
Etienne Burdet <etienne.burdet@gmail.com>
2020-12-17 06:50:03 +0100

    comment geodistance (not working)

--- src/routes/farms/_partials/ItemContent.svelte
+++ src/routes/farms/_partials/ItemContent.svelte
@@ -1,31 +1,33 @@
 <script>
-    export let fields;
+  export let fields;
 
-    const prettyPrintDistances = (meters) => {
-        if (meters > 1000) {
-            return (meters / 1000).toFixed(1) + 'km';
-        } else {
-            return (meters).toPrecision(0) + 'm.';
-        }
-    };
+  const prettyPrintDistances = (meters) => {
+    if (meters > 1000) {
+      return (meters / 1000).toFixed(1) + 'km';
+    } else {
+      return meters.toPrecision(0) + 'm.';
+    }
+  };
 </script>
 
 <p class="title is-size-5">{fields.nom.toString() != 'null' ? fields.nom : 'Nom non renseigné'}</p>
 <p class="subtitle is-size-6">
-  {fields.adresse.toString() != 'null' ? fields.adresse : 'Adresse non renseigné'}
-  {#if fields.geo_distance.toString() != 'null'} - <span class="distance">{'<' + prettyPrintDistances(fields.geo_distance)}</span>{/if}
+  <!-- {fields.adresse.toString() != 'null' ? fields.adresse : 'Adresse non renseigné'}
+  {#if fields.geo_distance.toString() != 'null'}
+    -
+    <span class="distance">{'<' + prettyPrintDistances(fields.geo_distance)}</span>
+  {/if} -->
 </p>
 <div class="tags">
-    <div class="tag is-medium">
-      {fields.nom_commune.toString() != 'null' ? fields.nom_commune : 'Commune non renseigné'}
-    </div>
+  <div class="tag is-medium">
+    {fields.nom_commune.toString() != 'null' ? fields.nom_commune : 'Commune non renseigné'}
+  </div>
 </div>
 
-
 <style lang="scss">
-    .subtitle {
-        .distance {
-            color: #000000;
-        }
+  .subtitle {
+    .distance {
+      color: #000000;
     }
+  }
 </style>
